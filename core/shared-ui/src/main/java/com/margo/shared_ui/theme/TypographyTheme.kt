035edbeeea5dd8aca02c97cc7edf08463fe2048d
pablomargo <pablomargolin@gmail.com>
2026-02-28 00:42:35 -0300

    Add new colors and change typographies

--- core/shared-ui/src/main/java/com/margo/shared_ui/theme/TypographyTheme.kt
+++ core/shared-ui/src/main/java/com/margo/shared_ui/theme/TypographyTheme.kt
@@ -10,9 +10,12 @@ import com.margo.shared_ui.themes.SpaceFlightTypographyTheme
 abstract class TypographyTheme {
     abstract val textBaseNormal: Typography
     abstract val textBaseNormalSmall: Typography
+    abstract val textBaseNormalXSmall: Typography
     abstract val textBaseBold: Typography
     abstract val textBaseBoldSmall: Typography
+    abstract val textBaseBlackBig: Typography
     abstract val textBaseBlack: Typography
+    abstract val textBaseBlackSmall: Typography
     abstract val textBoldItalic: Typography
     abstract val textBoldItalicSmall: Typography
     abstract val textBlackItalic: Typography
--- core/shared-ui/src/main/java/com/margo/shared_ui/themes/SpaceFlightColorTheme.kt
+++ core/shared-ui/src/main/java/com/margo/shared_ui/themes/SpaceFlightColorTheme.kt
@@ -4,8 +4,11 @@ import androidx.compose.ui.graphics.Color
 import com.margo.shared_ui.theme.ColorTheme
 
 val SpaceFlightColorTheme: ColorTheme = object: ColorTheme() {
-    override val primaryColor: BaseColor = BaseColor(color = Color(0xFF000000))
-    override val surfaceColor: BaseColor = BaseColor(color = Color(0xFFF8F7F4))
+    override val primaryColor: BaseColor = BaseColor(color = Color(0xFF3D4165))
+    override val blackColor: BaseColor = BaseColor(color = Color(0xFF000000))
+    override val surfaceColor: BaseColor = BaseColor(color = Color(0xFFFCFBFF))
+    override val surfaceLightColor: BaseColor = BaseColor(color = Color(0xFFF8F8F8))
     override val whiteColor: BaseColor = BaseColor(color = Color(0xFFFFFFFF))
     override val actionColor: BaseColor = BaseColor(color = Color(0xFF1C998E))
+    override val transparentColor: BaseColor = BaseColor(color = Color(0x00000000))
 }
